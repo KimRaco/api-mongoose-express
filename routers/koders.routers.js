4fe85c27816f43a57e36335e54ebc761c5e54eef
Kim Ramirez <kim.rc.1066@gmail.com>
2023-04-18 20:32:32 -0600

    feat: error custom added

--- routers/koders.routers.js
+++ routers/koders.routers.js
@@ -1,62 +1,109 @@
 import express from 'express';
 import { Koder } from '../models/koders.model.js'
+import { CustomError } from '../errorCustom.js'
 
 const router = express.Router()
 
 router.get('/',async (request, response) => {
-
-    const allKoders = await Koder.find({})
-
-    response.json({
-        sucess: true,
-         data: {
-            koders: allKoders
-         }
-        })
-})
-
-router.post('/',async (request, response) => {
-    const newKoder = {
-        name: 'post test',
-        lastName: 'Pacheco',
-        age: '28',
-        gender: 'h'
-    }
-
-    const koderCreated = await Koder.create(newKoder)
-
-    response.json({
-        sucess: true,
-         data: {
-            koders: koderCreated
-         }
-        })
-})
-
-router.patch('/', async (request, response) => {
-    const newData = {
-            name: 'update test'
-        }
-
-        const koderUpdated = await Koder.findByIdAndUpdate('643e1dfa4667c9806bc83abb', newData,{new:true})
+    try {
+        const allKoders = await Koder.find({})
+        
+        if(!allKoders)
+        throw new CustomError("Koders not found", 404)
         
         response.json({
             sucess: true,
              data: {
-                koders: koderUpdated
+                koders: allKoders
              }
             })
+
+    } catch (error) {
+        response
+        .status(error.status)
+        .json({
+            sucess: false,
+            message: error.message
+        })
+    }
 })
 
-router.delete('/', async (request, response) => {
-     const koderDeleted = await Koder.findByIdAndDelete('643e1dd1c84c485c48f21086')
+router.post('/',async (request, response) => {
+   
+    try{
         
-     response.json({
-        sucess: true,
-         data: {
-            koders: koderDeleted
-         }
+        const newKoder = request.body
+    
+        const koderCreated = await Koder.create(newKoder)
+
+        response
+        .status(201)
+        .json({
+            sucess: true,
+             data: {
+                koders: koderCreated
+             }
+            })
+   
+    } catch(error) {
+        response
+        .status(error.status || 400)
+        .json({
+            sucess: false,
+            message: error.message
         })
+    }
+
+})
+
+router.patch('/koders/:id', async (request, response) => {
+    
+    try{
+        
+        const { id } = request.params
+        const newData = request.body
+    
+        const koderUpdated = await Koder.findByIdAndUpdate(id, newData,{ new:true })
+            
+        response.json({
+            sucess: true,
+            data: {
+                koders: koderUpdated
+            }
+        })
+
+    } catch(error) {
+        response
+        .status(error.status || 400)
+        .json({
+            sucess: false,
+            message: error.message
+        })
+    } 
+})
+
+router.delete('/koders/:id', async (request, response) => {
+    
+    try {
+        const { id } = request.params
+    
+        const koderDeleted = await Koder.findByIdAndDelete(id)
+            
+         response.json({
+            sucess: true,
+             data: {
+                koders: koderDeleted
+             }
+        })
+
+    } catch(error) {
+        response
+        .status(error.status || 400)
+        .json({
+            sucess: false,
+            message: error.message
+        })
+    } 
 })
 
 export default router
